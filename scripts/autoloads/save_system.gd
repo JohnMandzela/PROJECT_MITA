1910e0556e652a9695dd21bd6236d71ddf08d358
Azmalent <dmitry.sjestov@yandex.ru>
2026-03-09 18:57:34 +0300

    Реализовать сохранение позиции игрока

--- scripts/autoloads/save_system.gd
+++ scripts/autoloads/save_system.gd
@@ -2,36 +2,43 @@ extends Node
 
 const SAVE_PATH := "user://save.bin"
 
-
-
 # TODO: перенести в меню
 func save_game() -> void:
 	var file := FileAccess.open(SAVE_PATH, FileAccess.WRITE)
 	
-	var player: Node = %Player
+	var player = GameManager.player
 	if not player:
 		print("Узел игрока не найден")
 		return
 		
-	var flashlight: Node = %Player/Phone_Flashlight
-		
-	var save_dict := {
-		"current_scene" = get_tree().current_scene.name,
+	var save_data := {
+		"current_scene" = get_tree().current_scene.name, # TODO
 		"player_position" = player.position,
 		"player_direction" = player.last_direction,
-		"flashlight_enabled" = flashlight.enabled
+		"flashlight_enabled" = player.flashlight.enabled
 	}
 	
-	print(save_dict)
+	file.store_var(save_data)
+	file.close()
+	
+	print("Игра сохранена")
 
 func load_game() -> void:
+	var player = GameManager.player
+	if not player:
+		print("Узел игрока не найден")
+		return
+		
 	var file := FileAccess.open(SAVE_PATH, FileAccess.READ)
 	if not file:
 		print("Файл с сохранением не найден")
 		return
-		
-	var player: Node = %Player
-	if not player:
-		print("Узел игрока не найден")
-		return
 	
+	var save_data: Dictionary = file.get_var()
+	file.close()
+	
+	player.position = save_data["player_position"]
+	player.last_direction = save_data["player_direction"]
+	player.flashlight.enabled = save_data["flashlight_enabled"]
+	
+	print("Игра загружена")
