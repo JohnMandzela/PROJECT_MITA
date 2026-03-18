699881530a16277077139e54764f196a584ab459
Azmalent <dmitry.sjestov@yandex.ru>
2026-03-19 02:22:54 +0300

    Кнопки сохранения и загрузки в телефоне

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -190,3 +190,11 @@ func _on_back_from_messenger_pressed() -> void:
 func _on_back_from_tutorial_pressed() -> void:
 	tutorial.visible = false
 	messenger.visible = true
+
+
+func _on_save_pressed() -> void:
+	SaveSystem.save_game()
+
+
+func _on_load_pressed() -> void:
+	SaveSystem.load_game()
